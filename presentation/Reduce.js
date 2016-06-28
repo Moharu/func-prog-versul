dc71bef288dcc257b84906ba9dc729a6364be6e8
Moharu <joaotelo.nh@hotmail.com>
2016-06-28 14:05:23 -0300

    e12

--- presentation/Reduce.js
+++ presentation/Reduce.js
@@ -1,18 +1,31 @@
 import expect from 'expect.js'
 
 it('Função REDUCE', () => {
-    // Provavelmente uma das funções de alta ordem mais importantes
-    // e mais versáteis.
-    // Com o REDUCE, é possível implementar Filter, Map, entre muitos outros
-    // Quando você não sabe qual a função que deve utilizar para modificar uma
-    // coleção, REDUCE provavelmente é a resposta certa.
-
     // Exemplo básico:
     // Somar todos os valores de um array
     const sum = (a,b) => a+b
     const values = [10,20,15,25,30]
 
     const totalValue = values.reduce(sum)
+
+    // Exemplo confuso? vamos entender melhor o reduce olhando sua documentação:
+    /*  The reduce() method applies a function against an accumulator and each
+        value of the array (from left-to-right) to reduce it to a single value.
+    arr.reduce(iterator[, initialValue])
+        iterator: Function(previousValue, currentValue[, currentIndex, array])
+    */
+
+    // Ou seja, o que reduce faz é aplicar a função definida em cada elemento
+    // do array, da esquerda para a direita, utilizando o return da última chamada
+    // como argumento da próxima.
+    // Para a primeira chamada da função, é possível especificar o valorInicial
+    // (na chamada do reduce). Se esse valor não é especificado, é utilizado o
+    // primeiro valor do array.
+
+    // Chamamos as funções no formato (estadoInicial, entrada) -> novoEstado
+    // de Funções Redutoras
+    // É fácil verificar que Sum é uma função redutora.
+
     expect(
         totalValue
     ).to.equal(
