79771ca7132d6d6934d1100aeba148313effd6ba
Moharu <joaotelo.nh@hotmail.com>
2016-06-28 10:19:52 -0300

    e6

--- presentation/HigherOrderFunctions.js
+++ presentation/HigherOrderFunctions.js
@@ -6,41 +6,47 @@ it('Funções de alta ordem', () => {
     const sub = (a,b) => a - b
     const mul = (a,b) => a * b
     const div = (a,b) => a / b
+    const mod = (a,b) => a % b
+    const pot = (a,b) => Math.pow(a,b)
 
-    // O quê você acha desse código?
-    const calculator = (a,b,op) => {
-        switch(op){
-            case 'sum':
-                return sum(a,b)
-            break;
-            case 'sub':
-                return sub(a,b)
-            break;
-            case 'mul':
-                return mul(a,b)
-            break;
-            case 'div':
-                return div(a,b)
-            break;
-        }
+    // que tal então...
+    const calculator = (a, b, operation) => {
+        return operation(a,b)
     }
 
-    // Ele funciona perfeitamente...
+    // Voilá!
     expect(
-        calculator(4,2,'sum')
+        calculator(4,2,sum)
     ).to.equal(6)
     expect(
-        calculator(4,2,'sub')
+        calculator(4,2,sub)
     ).to.equal(2)
     expect(
-        calculator(4,2,'mul')
+        calculator(4,2,mul)
     ).to.equal(8)
     expect(
-        calculator(4,2,'div')
+        calculator(4,2,div)
     ).to.equal(2)
+    expect(
+        calculator(4,2,mod)
+    ).to.equal(0)
+    expect(
+        calculator(4,2,pot)
+    ).to.equal(16)
 
-    // E se eu quiser implementar as funções módulo, resto, potencia, etc ?
-    // Ela vai ficar cada vez maior, e eu vou brincar de passar strings
-    // pra cá e pra lá.
+    // Nesse caso, calculator é uma função de alta ordem
+    // ou seja, ela tem como um de seus parâmetros, OUTRA função
+
+    // CUIDADO: Como javascript dá muita liberdade, precisamos ter cuidado
+    // para passar como argumento apenas funções que recebam dois argumentos (a,b)
+
+    // Já que economizamos bastante tempo codificando, podemos fazer uma pequena
+    // documentaçaozinha, para ajudar quem for debugar isso futuramente :)
+
+    /** calculator(n1,n2,f)
+            n1: Number
+            n2: Number
+            f: Function(Number, Number)
+    */
 
 })
