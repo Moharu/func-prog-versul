935369f898f12fe5ddaa757d83b0523dd1a85326
Moharu <joaotelo.nh@hotmail.com>
2016-06-28 14:19:15 -0300

    e13

--- presentation/Reduce.js
+++ presentation/Reduce.js
@@ -1,35 +1,39 @@
 import expect from 'expect.js'
 
+/*  The reduce() method applies a function against an accumulator and each
+value of the array (from left-to-right) to reduce it to a single value.
+arr.reduce(iterator[, initialValue])
+iterator: Function(previousValue, currentValue[, currentIndex, array])
+*/
 it('Função REDUCE', () => {
-    // Exemplo básico:
-    // Somar todos os valores de um array
-    const sum = (a,b) => a+b
-    const values = [10,20,15,25,30]
 
-    const totalValue = values.reduce(sum)
+    // Vamos olhar um novo exemplo com os amiguinhos
+    const animals = [
+        { name: 'José', species: 'bird'},
+        { name: 'Claudiovaldo', species: 'dog'},
+        { name: 'Boladepelo', species: 'cat'},
+        { name: 'Cusco', species: 'dog'},
+        { name: 'Godofredo', species: 'bird'},
+        { name: 'Garfield', species: 'cat'},
+        { name: 'Scooby', species: 'dog'}
+    ]
+    // Quero saber a quantidade de cachorros no array animals
+    //  OBS: Uma alternativa é utilizar Filter com .length
 
-    // Exemplo confuso? vamos entender melhor o reduce olhando sua documentação:
-    /*  The reduce() method applies a function against an accumulator and each
-        value of the array (from left-to-right) to reduce it to a single value.
-    arr.reduce(iterator[, initialValue])
-        iterator: Function(previousValue, currentValue[, currentIndex, array])
-    */
-
-    // Ou seja, o que reduce faz é aplicar a função definida em cada elemento
-    // do array, da esquerda para a direita, utilizando o return da última chamada
-    // como argumento da próxima.
-    // Para a primeira chamada da função, é possível especificar o valorInicial
-    // (na chamada do reduce). Se esse valor não é especificado, é utilizado o
-    // primeiro valor do array.
-
-    // Chamamos as funções no formato (estadoInicial, entrada) -> novoEstado
-    // de Funções Redutoras
-    // É fácil verificar que Sum é uma função redutora.
-
+    const isDog = (a) => a.species === 'dog'
+    const dogReducer = (count, animal) => {
+        return isDog(animal) ? count + 1 : count
+    }
+    const quantosDoge = animals.reduce(dogReducer, 0)
     expect(
-        totalValue
-    ).to.equal(
-        100
-    )
+        quantosDoge
+    ).to.equal(3)
+
+    // Forma reduzida:
+    const countDoges = animals.reduce((c,a) => a.species === 'dog'? c+1 : c, 0)
+    expect(
+        countDoges
+    ).to.equal(3)
+
 
 })
