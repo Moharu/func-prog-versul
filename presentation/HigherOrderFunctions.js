9f56b4d5fb2da7ee0c44036ccc06b4534dd13330
Moharu <joaotelo.nh@hotmail.com>
2016-06-28 10:05:33 -0300

    e5

--- presentation/HigherOrderFunctions.js
+++ presentation/HigherOrderFunctions.js
@@ -2,21 +2,45 @@ import expect from 'expect.js'
 
 it('Funções de alta ordem', () => {
 
-    // Uma função pura, simples e inocente
-    const soma = (a,b) => {
-        return a + b
+    const sum = (a,b) => a+b
+    const sub = (a,b) => a - b
+    const mul = (a,b) => a * b
+    const div = (a,b) => a / b
+
+    // O quê você acha desse código?
+    const calculator = (a,b,op) => {
+        switch(op){
+            case 'sum':
+                return sum(a,b)
+            break;
+            case 'sub':
+                return sub(a,b)
+            break;
+            case 'mul':
+                return mul(a,b)
+            break;
+            case 'div':
+                return div(a,b)
+            break;
+        }
     }
 
-    // Que certamente funciona
+    // Ele funciona perfeitamente...
     expect(
-        soma(1,2)
-    ).to.equal(
-        3
-    )
+        calculator(4,2,'sum')
+    ).to.equal(6)
     expect(
-        soma(5,5)
-    ).to.equal(
-        10
-    )
+        calculator(4,2,'sub')
+    ).to.equal(2)
+    expect(
+        calculator(4,2,'mul')
+    ).to.equal(8)
+    expect(
+        calculator(4,2,'div')
+    ).to.equal(2)
+
+    // E se eu quiser implementar as funções módulo, resto, potencia, etc ?
+    // Ela vai ficar cada vez maior, e eu vou brincar de passar strings
+    // pra cá e pra lá.
 
 })
